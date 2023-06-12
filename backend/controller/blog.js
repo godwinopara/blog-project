b0b366082b8418b5ff8a65298b5aef13acec257a
Godwin Opara <godwinopara62@yahoo.com>
2023-06-12 15:54:49 +0100

    convert .then to async/await

--- backend/controller/blog.js
+++ backend/controller/blog.js
@@ -6,13 +6,6 @@ const Blog = require("../model/blog");
 blogRouter.get("/", async (req, res) => {
 	const blogs = await Blog.find({});
 	res.json(blogs);
-	// Blog.find({})
-	// 	.then((blogs) => {
-	// 		res.json(blogs);
-	// 	})
-	// 	.catch((error) => {
-	// 		res.status(500).json({ error: error.message });
-	// 	});
 });
 
 // GET SINGLE BLOG POST
@@ -21,13 +14,6 @@ blogRouter.get("/:id", async (req, res) => {
 	const id = req.params.id;
 	const blog = await Blog.findById(id);
 	res.json(blog);
-	// Blog.findById(id).then((blog) => {
-	// 	if (blog) {
-	// 		res.json(blog);
-	// 	} else {
-	// 		res.status(404).json({ error: "Not Found" });
-	// 	}
-	// });
 });
 
 // DELETE BLOG POST
@@ -36,19 +22,6 @@ blogRouter.delete("/:id", async (req, res) => {
 	const id = req.params.id;
 	const blog = await Blog.findByIdAndDelete(id);
 	res.status(204).end();
-
-	// Blog.findByIdAndDelete(id)
-	// 	.then((blog) => {
-	// 		if (blog) {
-	// 			console.log(blog);
-	// 			res.status(204).end();
-	// 		} else {
-	// 			res.status(404).end();
-	// 		}
-	// 	})
-	// 	.catch((error) => {
-	// 		res.status(400).json(error);
-	// 	});
 });
 
 // ADD BLOG POST
@@ -60,14 +33,6 @@ blogRouter.post("/", async (req, res) => {
 
 	const newBlog = await blog.save();
 	res.status(201).json(newBlog);
-
-	// blog.save()
-	// 	.then((savedBlog) => {
-	// 		res.status(201).json(savedBlog);
-	// 	})
-	// 	.catch((error) => {
-	// 		res.status(400).json({ error: error.message });
-	// 	});
 });
 
 module.exports = blogRouter;

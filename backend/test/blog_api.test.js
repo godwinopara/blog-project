43ae6d783e26dff0d5840898869c9dd5ba513677
Godwin Opara <godwinopara62@yahoo.com>
2023-06-12 16:23:27 +0100

    wrote test for adding new blog

--- backend/test/blog_api.test.js
+++ backend/test/blog_api.test.js
@@ -28,6 +28,8 @@ beforeEach(async () => {
 	await blogObject.save();
 }, 10000);
 
+// TEST TO CHECK IF THE API IS IN "APPLICTION/JSON FORMAT"
+
 test("blog list are returned as json", async () => {
 	await api
 		.get("/api/blogs")
@@ -35,11 +37,15 @@ test("blog list are returned as json", async () => {
 		.expect(200);
 }, 10000);
 
+// TEST TO CHECK IF BLOG POST ARE COMPLETE
+
 test("all blogs are required", async () => {
 	const response = await api.get("/api/blogs");
 
 	expect(response.body).toHaveLength(initialBlogs.length);
 }, 10000);
+
+// TEST THAT RETURNS THE FIRST BLOG POST
 
 test("the first blog", async () => {
 	const response = await api.get("/api/blogs");
@@ -47,10 +53,36 @@ test("the first blog", async () => {
 	expect(response.body[0].title).toBe("Modern Javascript");
 }, 10000);
 
+// TEST FOR CHECKING IF BLOGS CONTAINS A PARTICULAR BLOG
+
 test("a specific blog is return ", async () => {
 	const response = await api.get("/api/blogs");
 	const contents = response.body.map((r) => r.title);
 	expect(contents).toContain("Html is easy");
+});
+
+// TEST FOR ADDING NEW BLOG
+
+test("a valid blog can be added", async () => {
+	const newBlog = {
+		title: "async/await simplifies making async calls",
+		author: "James simpson",
+		url: "https://github.com",
+		likes: 10,
+	};
+
+	await api
+		.post("/api/blogs")
+		.send(newBlog)
+		.expect(201)
+		.expect("Content-Type", /application\/json/);
+
+	const response = await api.get("/api/blogs");
+
+	const contents = response.body.map((r) => r.title);
+
+	expect(response.body).toHaveLength(initialBlogs.length + 1);
+	expect(contents).toContain("async/await simplifies making async calls");
 });
 
 afterAll(async () => {
